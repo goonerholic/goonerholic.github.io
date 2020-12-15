3dcffb1b7c1db7c495689080b2a72343caa5b589
goonerholic <goonerholic@gmail.com>
2020-12-16 01:12:04 +0900

    change style

--- src/templates/PostListPage.tsx
+++ src/templates/PostListPage.tsx
@@ -1,14 +1,11 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { graphql } from 'gatsby';
-import { useMemo } from 'react';
 import { PostListQuery } from '../../gatsby-graphql';
 import PostList from '../components/blog/PostList';
-import PostListItem from '../components/blog/PostListItem';
 import ContentWrapper from '../components/common/ContentWrapper';
 import Header from '../components/common/Header';
 import Layout from '../components/common/Layout';
-import PostPreview from '../components/home/PostPreview';
 
 interface PostListProps {
   pageContext: {
@@ -20,23 +17,9 @@ interface PostListProps {
   data: PostListQuery;
 }
 
-const style = css``;
+// const style = css``;
 
 export default function PostListPage({ pageContext, data }: PostListProps) {
-  console.log(data);
-  console.log(pageContext);
-  // const { currentPage, numPages } = pageContext;
-  // const isFirst = useMemo(() => currentPage === 1, [currentPage]);
-  // const isLast = useMemo(() => currentPage === numPages, [
-  //   currentPage,
-  //   numPages,
-  // ]);
-  // const prevPage = useMemo(
-  //   () => (currentPage - 1 === 1 ? '/' : `${currentPage - 1}`),
-  //   [currentPage],
-  // );
-  // const nextPage = useMemo(() => `/${currentPage + 1}`, [currentPage]);
-
   return (
     <Layout>
       <Header />
