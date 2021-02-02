ce9142fb68835973be44fab2020f34ca17b430d9
goonerholic <goonerholic@gmail.com>
2021-02-02 09:26:48 +0900

    add sitemap, robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,6 +125,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/amdin`],
+      },
+    },
+    `gatsby-plugin-robots-txt`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
