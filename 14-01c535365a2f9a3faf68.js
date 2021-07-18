81f4e6455a21bd0aadcd9249230661efbb5c6ed8
goonerholic <goonerholic@users.noreply.github.com>
2021-07-18 23:27:19 +0000

    deployed via Gatsby Publish Action 🎩 for 4e99ecd5698f85eb0337770ffbeafcd0e7b52d8d

---
+++ 14-01c535365a2f9a3faf68.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{"1L5P":function(e,t,l){var n=l("q1tI");function o(e){return n.createElement("svg",e,[n.createElement("title",{key:0},"HTML5 icon"),n.createElement("path",{d:"M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z",key:1})])}o.defaultProps={role:"img",viewBox:"0 0 24 24"},e.exports=o,o.default=o}}]);
+//# sourceMappingURL=14-01c535365a2f9a3faf68.js.map
